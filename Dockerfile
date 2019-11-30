61e589bc3ca25ffeb64b92b490c687356bdd5c1f
ghajduk3 <hajdukovicgojko3@gmail.com>
2019-11-30 18:17:04 +0100

    zvezna

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ WORKDIR /app
 
 ADD ./api/target/e-scooter-orders-api-1.0-SNAPSHOT.jar /app
 
-EXPOSE 8081
+EXPOSE 8086
 
 CMD ["java", "-jar", "e-scooter-orders-api-1.0-SNAPSHOT.jar"]
--- api/src/main/java/si/fri/rso/escooterental/escooterorders/api/v1/resources/EscooterOrdersResource.java
+++ api/src/main/java/si/fri/rso/escooterental/escooterorders/api/v1/resources/EscooterOrdersResource.java
@@ -30,8 +30,16 @@ public class EscooterOrdersResource {
 
     @GET
     public Response getEscooterOrder(){
-        List<EscooterOrders> orders = escooterBean.getEscooterOrders();
-        return Response.status(Response.Status.OK).entity(orders).build();
+//        List<EscooterOrders> orders = escooterBean.getEscooterOrders();
+//        return Response.status(Response.Status.OK).entity(orders).build();
+        return Response.status(Response.Status.OK).entity("{\n" +
+                "    \"clani\": [\"jm1234\", \"mn3322\"],\n" +
+                "    \"opis_projekta\": \"Nas projekt implementira aplikacijo za oddajo nepremicnin.\",\n" +
+                "    \"mikrostoritve\": [\"http://35.189.96.118:8081/v1/orders\", \"http://35.197.209.159:8080/v1/customers/\"],\n" +
+                "    \"github\": [\"https://github.com/jmezna/rso-customers\", \"https://github.com/jmezna/rso-orders\"],\n" +
+                "    \"travis\": [\"https://travis-ci.org/jmezna/rso-customers\", \"https://travis-ci.org/jmezna/rso-orders\"],\n" +
+                "    \"dockerhub\": [\"https://hub.docker.com/r/jmezna/rso-customers/\", \"https://hub.docker.com/r/jmezna/rso-orders/\"]\n" +
+                "}").build();
     }
     @GET
     @Path("/{EscooterId}")
